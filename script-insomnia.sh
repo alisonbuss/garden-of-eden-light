371f02ad1139d788a035c727e1cdf227f5cc54b0
alisonbuss <alisonbuss.dev@gmail.com>
2025-11-17 22:32:07 -0300

    Ajustes aplicados

--- script-insomnia.sh
+++ script-insomnia.sh
@@ -7,18 +7,16 @@
 #       bash script-insomnia.sh
 #-------------------------------------------------------------#
 
-if command -v insomnia-desktop >/dev/null 2>&1; then
-    echo "✅ Program(insomnia-desktop) already installed!";
-    echo -e "--> Directory:" "$(command -v insomnia-desktop)" "\n";
+if command -v insomnia >/dev/null 2>&1; then
+    echo "✅ Program(Insomnia) already installed!";
+    echo -e "--> Directory:" "$(command -v insomnia)" "\n";
 else
-    echo -e "📦 Installing the program: insomnia-desktop...\n";
+    echo -e "📦 Installing the program: Insomnia...\n";
 
     wget "https://updates.insomnia.rest/downloads/ubuntu/latest?&app=com.insomnia.app&source=website" -O ~/Downloads/insomnia-desktop.deb;
 
     sudo dpkg -i ~/Downloads/insomnia-desktop.deb;
     sudo apt install -f;
-
-    sleep 1s;
 fi
 
 exit 0;
